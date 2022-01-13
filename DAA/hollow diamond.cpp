f32c88336f15078b65b332e090f88365578d25bf
Mukul-82 <87289461+Mukul-82@users.noreply.github.com>
2022-01-13 20:59:24 +0530

    Update hollow diamond.cpp

--- DAA/hollow diamond.cpp
+++ DAA/hollow diamond.cpp
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,p=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            printf(" ");
+        }
+        printf("%d",i);
+        for(int k=1;k<i;k++)
+        {
+                printf(" ");
+        }
+
+        for(int j=1;j<=i-2;j++)
+        {
+            printf(" ");
+        }
+        if(i!=1)
+        {
+            printf("%d",i);
+        }
+
+
+        printf("\n");
+    }
+}
+
+
 
