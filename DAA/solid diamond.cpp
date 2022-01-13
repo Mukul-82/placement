0fcb1999d021a2fb3b5398d9a2d6a740d8b8da1d
Mukul-82 <87289461+Mukul-82@users.noreply.github.com>
2022-01-13 20:52:55 +0530

    Update solid diamond.cpp

--- DAA/solid diamond.cpp
+++ DAA/solid diamond.cpp
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=(n/2)+1;i++)
+    {
+        for(int j=i;j<(n/2)+1;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=n/2;i++)
+    {
+        for(int k=1;k<=i;k++)
+        {
+            printf(" ");
+        }
+        for(int j=n/2;j>=i;j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+
+
 
