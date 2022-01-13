f44cb6d8f553b83991eae0afcc315831739e9b10
Mukul-82 <87289461+Mukul-82@users.noreply.github.com>
2022-01-13 21:03:12 +0530

    Update fibonacci half pyramid.cpp

--- DAA/fibonacci half pyramid.cpp
+++ DAA/fibonacci half pyramid.cpp
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,s,a=0,b=1;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            s=a+b;
+            a=b;
+
+            printf("%d ",b);
+            b=s;
+        }
+        printf("\n");
+    }
+}
 
