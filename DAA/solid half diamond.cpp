b0c266e8e755800eb9139db7b6ace00beb2b109a
Mukul-82 <87289461+Mukul-82@users.noreply.github.com>
2022-01-13 20:56:42 +0530

    Update solid half diamond.cpp

--- DAA/solid half diamond.cpp
+++ DAA/solid half diamond.cpp
@@ -1,1 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,k=n;
+    scanf("%d",&n);
+    for(int i=n/2;i>=0;i--)
+    {
+        for(int j=n/2;j>=i;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    for(int i=0;i<n/2;i++)
+    {
+        for(int j=n/2;j>i;j--)
+        {
+
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
 
